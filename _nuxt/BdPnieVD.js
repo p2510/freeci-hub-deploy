21e801c7598886e3f02eecd814fb5b4db604fa38
YATACHI <60470788+p2510@users.noreply.github.com>
2024-06-07 14:42:11 +0200

    update

---
+++ _nuxt/BdPnieVD.js
@@ -0,0 +1,1 @@
+import{g as l,i as c,al as o,h as i}from"./CXEG3h5d.js";const m=()=>{const u=l(),s=c(),a=o("userinfo",()=>null),r=o("error",()=>null),n=async()=>{if(s.value)try{const{data:e,error:t}=await u.from("usersinfo").select("*").eq("email",s.value.email);if(t)throw t;a.value=e.length?e[0]:null}catch(e){r.value=e.message}};return i(()=>{n()}),{userInfo:a,error:r}};export{m as u};
---
+++ _nuxt/DIjWWSAA.js
@@ -0,0 +1,1 @@
+import"./CXEG3h5d.js";const r=""+new URL("logo1.BUBA-uIA.png",import.meta.url).href;export{r as _};
