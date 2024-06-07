a467fef8fa3cc02474f9d107cf38fb93c6a91b6d
YATACHI <60470788+p2510@users.noreply.github.com>
2024-06-07 14:00:49 +0200

    update

---
+++ _nuxt/BUeHN018.js
@@ -0,0 +1,1 @@
+import{i as t,s as r,v as s,x as n,c,o as u,n as i}from"./DW60q1GZ.js";const _={__name:"confirm",setup(l){const e=t(),a=r().public.supabase.cookieName,o=s(`${a}-redirect-path`).value;return n(e,()=>{if(e.value)return s(`${a}-redirect-path`).value=null,i(o||"/dashboard")},{immediate:!0}),(m,p)=>(u(),c("div",null,"Connexion en cours..."))}};export{_ as default};
