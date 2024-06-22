8d4abf30ac00d40b3862975d49db6ce69daafade
YATACHI <60470788+p2510@users.noreply.github.com>
2024-06-22 16:13:43 +0200

    update

---
+++ _nuxt/BGAHOhI9.js
@@ -0,0 +1,1 @@
+import"./hVa5h8aK.js";const r=""+new URL("logo1.BUBA-uIA.png",import.meta.url).href;export{r as _};
---
+++ _nuxt/DhSerGN9.js
@@ -0,0 +1,1 @@
+import{m as t,n as r,q as s,s as n,c,o as u,v as i}from"./hVa5h8aK.js";const _={__name:"confirm",setup(l){const e=t(),a=r().public.supabase.cookieName,o=s(`${a}-redirect-path`).value;return n(e,()=>{if(e.value)return s(`${a}-redirect-path`).value=null,i(o||"/dashboard")},{immediate:!0}),(m,p)=>(u(),c("div",null,"Connexion en cours..."))}};export{_ as default};
