5376f9d737e1749289e6926252925d2230c5bb3e
YATACHI <60470788+p2510@users.noreply.github.com>
2024-07-20 04:14:23 +0200

    update

---
+++ _nuxt/2na7B64W.js
@@ -0,0 +1,1 @@
+import"./RsG6uWRm.js";const r=""+new URL("logo1.BUBA-uIA.png",import.meta.url).href;export{r as _};
---
+++ _nuxt/Ds_Il6gc.js
@@ -0,0 +1,1 @@
+import{g as l,m as c,aj as o,E as i}from"./C8vjBnMy.js";const m=()=>{const u=l(),s=c(),a=o("userinfo",()=>null),r=o("error",()=>null),n=async()=>{if(s.value)try{const{data:e,error:t}=await u.from("usersinfo").select("*").eq("email",s.value.email);if(t)throw t;a.value=e.length?e[0]:null}catch(e){r.value=e.message}};return i(()=>{n()}),{userInfo:a,error:r}};export{m as u};
